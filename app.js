4ef9cace484ade19519bcd90029c542d5026cd19
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-08-19 15:13:19 +0600

    hi

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log(Math.pow(4, 0.5)+"spiderman"+"987"+Math.sqrt(9))
+console.log(Math.floor(Math.random()*90000) + 10000)
+console.log(!isNaN("10X"))
+const product = 5; const price = "7"; 
+const subTotal = product * price; 
+const tax = subTotal/10; 
+const total = subTotal + tax;
+console.log(total);
+console.log(subTotal);
+
+const category="pen"; 
+const performer = category+"-teller";
+console.log(performer)
